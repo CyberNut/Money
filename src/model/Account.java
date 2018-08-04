5df8ae2be2e48e409802bf30849a9c80f81deff5
CyberNut <cy6ernut@gmail.com>
2018-08-04 21:07:54 +0300

    Новый учебный проект "Money".

---
+++ src/model/Account.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Account {
+
+    private String login;
+    private User user;
+    private MoneyType type;
+
+}
---
+++ src/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+public class User {
+
+    private String login;
+    private String name;
+
+    public User(String login, String name) {
+        this.login = login;
+        this.name = name;
+    }
+
+
+}
